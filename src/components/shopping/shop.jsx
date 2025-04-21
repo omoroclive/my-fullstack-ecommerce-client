68c336759f194b74d3c82ff6623084298f0fc772
omoroclive <omoroclive16@gmail.com>
2025-04-21 21:02:28 +0300

    frontend modification

--- src/components/shopping/shop.jsx
+++ src/components/shopping/shop.jsx
@@ -10,7 +10,7 @@ const Shop = () => {
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const API_URL = process.env.REACT_APP_API_URL || "https://grateful-adventure-production.up.railway.app";
+  const API_URL = import.meta.env.VITE_API_URL  || "https://grateful-adventure-production.up.railway.app";
 
 useEffect(() => {
   const fetchProducts = async () => {
